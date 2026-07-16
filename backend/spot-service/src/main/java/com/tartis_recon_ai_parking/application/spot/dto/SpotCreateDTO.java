0edf0d181ceaf808aa417185019da3ef8964bc27
Ramidor <raulpeberota@gmail.com>
2026-07-17 00:36:03 +0200

    Creacion de DTO, UseCase y Bean

--- backend/spot-service/src/main/java/com/tartis_recon_ai_parking/application/spot/dto/SpotCreateDTO.java
+++ backend/spot-service/src/main/java/com/tartis_recon_ai_parking/application/spot/dto/SpotCreateDTO.java
@@ -1,5 +1,22 @@
 package com.tartis_recon_ai_parking.application.spot.dto;
 
+import com.tartis_recon_ai_parking.domain.spot.VehicleType;
+
 public class SpotCreateDTO {
- public String name;
+
+    private final VehicleType type;
+    private final Integer numSpot;
+
+    public SpotCreateDTO(VehicleType type, Integer numSpot) {
+        this.type = type;
+        this.numSpot = numSpot;
+    }
+
+    public VehicleType getType() {
+        return type;
+    }
+
+    public Integer getNumSpot() {
+        return numSpot;
+    }
 }
--- backend/spot-service/src/main/java/com/tartis_recon_ai_parking/infrastructure/spot/adapter/input/rest/dto/response/SpotResponse.java
+++ backend/spot-service/src/main/java/com/tartis_recon_ai_parking/infrastructure/spot/adapter/input/rest/dto/response/SpotResponse.java
@@ -1,5 +1,36 @@
 package com.tartis_recon_ai_parking.infrastructure.spot.adapter.input.rest.dto.response;
 
+import com.tartis_recon_ai_parking.domain.spot.SpotStatus;
+import com.tartis_recon_ai_parking.domain.spot.VehicleType;
+import java.util.UUID;
+
 public class SpotResponse {
 
+    private final UUID id;
+    private final VehicleType type;
+    private final Integer numSpot;
+    private final SpotStatus status;
+
+    public SpotResponse(UUID id, VehicleType type, Integer numSpot, SpotStatus status) {
+        this.id = id;
+        this.type = type;
+        this.numSpot = numSpot;
+        this.status = status;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public VehicleType getType() {
+        return type;
+    }
+
+    public Integer getNumSpot() {
+        return numSpot;
+    }
+
+    public SpotStatus getStatus() {
+        return status;
+    }
 }
